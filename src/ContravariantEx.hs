bfa09d6f1e6519f3be8af99f3f5f66edb4a4411e
J. Eduardo V. Morango <edvmorango@gmail.com>
2019-04-26 01:37:17 -0300

    wip: Op

--- src/ContravariantEx.hs
+++ src/ContravariantEx.hs
@@ -1,6 +1,8 @@
 -- https://ocharles.org.uk/blog/guest-posts/2013-12-21-24-days-of-hackage-contravariant.html
 module ContravariantEx where
 
+import           Data.Set
+
 {-
 
 (->) :: * -> * -> *
@@ -27,6 +29,12 @@ f <$> (((->) Int) Int) = ((->) Int) f Int
 
 ((->) Int) String =  (Int -> String) . (a -> Int)
 
+
+`a` inside `((->) a)` is part of the functorial structure
+
+so functors are limited to map the implicit `b`
+
+
 -}
 arrowG :: Int -> String
 arrowG = show
@@ -36,3 +44,29 @@ arrowF = (* 2)
 
 class Contravariant f where
   contramap :: (b -> a) -> f a -> f b
+
+{-
+
+-}
+newtype Op b a =
+  Op (a -> b)
+
+{-
+contramap (b -> a) (Op  (a -> b))
+contramap f (Op g) = g . f
+contramap (b -> a) (Op (a -> b)) =  (a -> b) (b -> a)
+
+(a -> b)  (b -> a) = b -> b
+
+-}
+instance Contravariant (Op z) where
+  contramap f (Op g) = Op (g . f)
+
+{-
+  Op Int [a] = Op ([a] -> Int)
+-}
+cLength :: Op Int [a]
+cLength = Op Prelude.length
+
+setToList :: Op Int (Set a)
+setToList = contramap toList cLength
